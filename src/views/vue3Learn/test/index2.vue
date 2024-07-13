e2b8b24dc7e81b59a81694c2fd3eb1aa3ecb7529
homeScl <419863966@qq.com>
2024-07-13 10:16:00 +0800

    修改缩进空格

--- src/views/vue3Learn/test/index2.vue
+++ src/views/vue3Learn/test/index2.vue
@@ -1,10 +1,9 @@
 <template>
-    <div>{{ a }}</div>
+    <div></div>
 </template>
 
 <script setup lang="ts">
 import { ref, reactive } from "vue"
-const a = ref(10086)
 </script>
 
 <style scoped></style>
