c706b1435a16ddf93ae7fdb65483694e7577d481
homeScl <419863966@qq.com>
2024-08-01 10:08:53 +0800

    修改页脚

--- src/layouts/components/Footer/index.vue
+++ src/layouts/components/Footer/index.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="footer flx-center">
-        <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © Geeker-Admin By Geeker Technology. </a>
+        <!-- <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © Geeker-Admin By Geeker Technology. </a> -->
+        <p>2022 © Geeker-Admin By Geeker Technology.</p>
     </div>
 </template>
 
